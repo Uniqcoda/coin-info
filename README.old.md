d8d9fb2c467c6f483cbba95f68108a190eb06c17
Uniqcoda <etokwudomary@gmail.com>
2022-02-09 23:38:40 +0100

    setup boilerplate

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# coin-info
+A platform that displays cryptocurrencies and some information about eacg coin
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <div className='cryptoHeader'></div>
+            <div className='cryptoDisplay'></div>
+        </div>
+    );
+}
+
+export default App;
